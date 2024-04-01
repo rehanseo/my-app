7f8b5fa86a66ad21474e7051fb9bf1c82e338560
rehan <rehan.saeed101@gmail.com>
2024-04-02 00:37:34 +0500

    All Done with three modules

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -42,13 +42,13 @@ export default function TextForms(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='dark'?'grey':'white', color:props.mode==='dark'?'white':'grey'}} onChange={HandleOnChange} id="myBox" rows="8"></textarea>
                 </div>
-                <button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className='btn btn-primary mx-1' onClick={handleloClick}>Convert to Lowercase</button>
-                <button className='btn btn-primary mx-1' onClick={handleSentenceCaseClick}>Convert to Sentence Case</button>
+                <button disabled={text.length===0} className='btn btn-primary mx-1 my-1' onClick={handleUpClick}>Convert to Uppercase</button>
+                <button disabled={text.length===0} className='btn btn-primary mx-1 my-1' onClick={handleloClick}>Convert to Lowercase</button>
+                <button disabled={text.length===0} className='btn btn-primary mx-1 my-1' onClick={handleSentenceCaseClick}>Convert to Sentence Case</button>
             </div>
             <div className='container my-3' style={{color:props.mode==='dark'?'white':'black'}}>
                 <h1>Your Text Summary</h1>
-                <p>{text.split(/\s+/).length} words and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
                 <p>{calculateReadingTime()} minute(s) read</p>
                 <h2>Preview</h2>
                 <p>{text}</p>
